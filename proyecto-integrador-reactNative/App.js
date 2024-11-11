812e7c3c85787156591391fef517bce708608739
Pilitore <142538235+Pilitore@users.noreply.github.com>
2024-11-11 20:38:54 -0300

    Algo de navegacion y dos componentes

--- proyecto-integrador-reactNative/App.js
+++ proyecto-integrador-reactNative/App.js
@@ -6,6 +6,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Register from './src/Screens/Register';
 import Home from './src/Screens/Home';
 import Login from './src/Screens/Login';
+import Profile from './src/Components/Profile';
+import HomeMenu from './src/Components/HomeMenu';
 
 const Stack = createNativeStackNavigator(); // Definir el Stack
 
@@ -15,7 +17,10 @@ export default function App() {
       <Stack.Navigator >
         <Stack.Screen name="Register" component={Register}/>
         <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name='Login' component={Login}/>
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Profile" component={Profile}/>
+        <Stack.Screen name="HomeMenu" component={HomeMenu}/>
+
       </Stack.Navigator>
 
     </NavigationContainer>
---
+++ proyecto-integrador-reactNative/src/Components/HomeMenu.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+class HomeMenu extends Component {
+
+}
+
+export default HomeMenu
