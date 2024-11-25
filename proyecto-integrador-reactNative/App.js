de96aee9bac1b9b749b54e753848716102e9beaa
vickystutzbach <mstutzbach@udesa.edu.ar>
2024-11-25 17:56:14 -0300

    cambios de clase

--- proyecto-integrador-reactNative/App.js
+++ proyecto-integrador-reactNative/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { auth } from './src/firebase/config'; 
 import Login from './src/screens/Login';
 import Profile from './src/components/Profile';
 import NewPost from './src/screens/NewPost';
@@ -17,19 +16,16 @@ export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      
     <Stack.Screen name="Login" component={Login}/>
     <Stack.Screen name="Register" component={Register}/>
-
-      <Stack.Screen name="Profile" component={Profile}/>
-      <Stack.Screen name="HomeMenu" component={HomeMenu}/>
-      <Stack.Screen name="NewPost" component={NewPost}/>
-      <Stack.Screen name="Post" component={Post}/> 
-      <Stack.Screen name="SearchUser" component={SearchUser}/>
+    <Stack.Screen name="Profile" component={Profile}/>
+    <Stack.Screen name="HomeMenu" component={HomeMenu}/>
+    <Stack.Screen name="NewPost" component={NewPost}/>
+    <Stack.Screen name="Post" component={Post}/> 
+    <Stack.Screen name="SearchUser" component={SearchUser}/>
 
     </Stack.Navigator>
-  </NavigationContainer>
-
+    </NavigationContainer>
 
   )  
 }
